0a11d9ffa068e2d99430d782ca104f1755a865ad
ThisisCool999 <someonesmart3@outlook.com>
2026-06-02 22:16:08 +0800

    Add files via upload

---
+++ renzao_guang/serve.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const ROOT = __dirname;
+const PORT = 8120;
+const TYPES = {
+  ".html": "text/html; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".svg": "image/svg+xml",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".woff2": "font/woff2",
+};
+
+http.createServer((req, res) => {
+  let urlPath = decodeURIComponent(req.url.split("?")[0]);
+  if (urlPath === "/") urlPath = "/index.html";
+  const filePath = path.join(ROOT, path.normalize(urlPath));
+  if (!filePath.startsWith(ROOT)) {
+    res.writeHead(403);
+    return res.end("forbidden");
+  }
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      return res.end("404 not found");
+    }
+    const type = TYPES[path.extname(filePath).toLowerCase()] || "application/octet-stream";
+    res.writeHead(200, { "Content-Type": type, "Cache-Control": "no-cache" });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log("renzao-guang serving on http://localhost:" + PORT));
